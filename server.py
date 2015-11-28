fc2f889201c10a1ef337dd05beea1b36941e95a9
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-28 13:15:53 +0100

    GPIO integration & "/on" endpoint

--- server.py
+++ server.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import lib.gpio.RPi.GPIO as GPIO
+import time
 
+# Flask App
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+# GPIO Pins
+LED_PIN = 12
+
+@app.route("/", methods=["GET"])
+def root():
     return "", 418
+
+@app.route("/on", methods=["GET", "POST"])
+def on():
+    on = is_on()
+
+    if request.method == "POST":
+        return "", 200
+    else:
+        return jsonify(state=on), 200
+
+def is_on():
+    GPIO.setup(LED_PIN, GPIO.IN)
+
+    timeout = time.time() + 4
+    while time.time() <= timeout:
+        if GPIO.input(LED_PIN):
+            return True
+
+    return False
 
 if __name__ == "__main__":
     app.run()
--- tests/server_test.py
+++ tests/server_test.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 import server
-import requests
+from unittest.mock import patch, call, MagicMock
+import json
+import lib.gpio.RPi.GPIO as GPIO
 
-class TestUtils(TestCase):
+class TestCoffeeWebservice(TestCase):
     def setUp(self):
         self.app = server.app.test_client()
 
@@ -10,3 +12,33 @@ class TestUtils(TestCase):
         request = self.app.get("/")
 
         self.assertEquals(418, request.status_code)
+
+    @patch('lib.gpio.RPi.GPIO.setup', autospec=True)
+    @patch('lib.gpio.RPi.GPIO.input', autospec=True)
+    def test_get_on_when_off(self, mocked_gpio_input, mocked_gpio_setup):
+        mocked_gpio_input.return_value = False
+
+        request = self.app.get("/on")
+
+        mocked_gpio_setup.assert_has_calls([call(12, GPIO.IN)])
+
+        self.assertEquals(200, request.status_code)
+        self.assertEquals(False, json.loads(request.get_data(as_text=True))["state"])
+
+    @patch('lib.gpio.RPi.GPIO.setup', autospec=True)
+    @patch('lib.gpio.RPi.GPIO.input', autospec=True)
+    def test_get_on_when_on(self, mocked_gpio_input, mocked_gpio_setup):
+        mocked_gpio_input.return_value = True
+
+        request = self.app.get("/on")
+
+        mocked_gpio_setup.assert_has_calls([call(12, GPIO.IN)])
+
+        self.assertEquals(200, request.status_code)
+        self.assertEquals(True, json.loads(request.get_data(as_text=True))["state"])
+
+    # @patch('lib.gpio.RPi.GPIO')
+    # def test_on_when_off(self, mocked_gpio):
+    #     request = self.app.post("/on")
+    #
+    #     self.assertEquals(200, request.status_code)
